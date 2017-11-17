21fbac7c198622ec000b28513c8d6f27fe7d05a0
Osvaldo Luna <osvaldounam18@gmail.com>
2017-11-16 19:34:22 -0600

    Se pide una serie de numero y se detiene hasta que se ingrese un 0, y se saca el promedio de estos

---
+++ Promedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            float  prom = 0;
+            do
+            {
+                Console.WriteLine("Numero: ");
+                float num = float.Parse(Console.ReadLine());
+                prom = prom + num;
+
+                if (num == 0)
+                    break;
+                else
+                    c++;
+            } while (true);
+
+            Console.WriteLine("El pormedio de estos es:" + (prom / c));
+            Console.ReadKey();
+        }
+    }
+}
